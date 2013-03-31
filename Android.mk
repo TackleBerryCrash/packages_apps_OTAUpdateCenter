5adfe891d39c07c872c6229d19318d28c51e560f
tackleberry_crash <amras.tasartir77@gmail.com>
2013-03-31 23:11:32 +0300

    OTA Capability
    Change-Id: I09030202851882c3840bda5c8c0ae05636a4d8cc

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 gcm
 LOCAL_PACKAGE_NAME := OTAUpdateCenter
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_PROGUARD_FLAG_FILES := proguard.cfg
 
